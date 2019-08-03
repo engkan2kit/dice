934c66825b5b803c627c0ac5d698c1ac749defb9
kmvligaray <karla.michelle.ligaray@gmail.com>
2019-08-03 19:23:16 +0900

    [EDIT] documentations for generateArrangements.

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 import random
 import numpy as np
 
-def nameOfFunction(N):
+def generateArrangements(N):
     """
-    Documentation here
+    This function accepts a number N greater than or equal to 3 
+    and returns an NxN matrix containing a random arrangements 
+    of integers 0-((N^2)-1).
+
+    
+
+    usage: generateArrangements(4) 
+    returns: [[1,4,7,5], [0,2,9,11], [12,15,3,6], [13,8,10,14]]
     """
     if N<3:
         return None
